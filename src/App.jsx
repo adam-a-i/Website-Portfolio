d4e197754f35141c74205a8a6c8d86e1a73fbb7b
Adam Ibrahim <adamyehialy@gmail.com>
2024-12-21 21:49:23 +0400

    compatibility

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,31 @@
-// import { useState } from "react";
-import './styles/general.css'
-import Navbar from './components/Navbar.jsx';
+import React from "react";
+import './styles/general.css';
+import Navbar from './components/Navbar';
 import Home from './components/Home';
-import Education from './components/Education.jsx';
+import Education from './components/Education';
 import AnimatedCursor from "react-animated-cursor";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
 function App() {
+  const isMobile = /Android|iPhone|iPad|iPod|Opera Mini|IEMobile|WPDesktop/i.test(
+    navigator.userAgent
+  );
+
   return (
-      <Router>
+    <Router>
       <div className="container">
         <Navbar/>
         <Routes>
-        <Route path="/" element={<Home/>} /> 
-        <Route path="/education" element={<Education/>} /> 
+          <Route path="/" element={<Home/>} /> 
+          <Route path="/education" element={<Education/>} /> 
         </Routes>
       </div>
-      <AnimatedCursor innerSize={10} outerSize={20}color="#0b0ee6"   outerAlpha={0.95}   innerScale={1}        outerScale={1}       showSystemCursor={false} innerStyle={{ backgroundColor: "rgba(50, 1, 128, 0.96)" }}outerStyle={{backgroundColor: "rgba(97, 15, 230, 0.96)",  zIndexndex : 1100}}/>
-      </Router>
-  )
+      {!isMobile && (
+        <AnimatedCursor innerSize={10} outerSize={20} color="#0b0ee6" outerAlpha={0.95} innerScale={1} outerScale={1} showSystemCursor={false} innerStyle={{ backgroundColor: "rgba(50, 1, 128, 0.96)" }} outerStyle={{ backgroundColor: "rgba(97, 15, 230, 0.96)", zIndex: 1100 }}
+        />
+      )}
+    </Router>
+  );
 }
 
-export default App
+export default App;

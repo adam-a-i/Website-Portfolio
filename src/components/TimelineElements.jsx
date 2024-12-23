ddffc2ec189f2a117638c2a197529c3b0f62443b
Adam Ibrahim <adamyehialy@gmail.com>
2024-12-23 13:02:02 +0400

    pull

--- src/components/TimelineElements.jsx
+++ src/components/TimelineElements.jsx
@@ -15,7 +15,7 @@ const TimelineElements = [
       location: "United Arab Emirates, Abu Dhabi",
       description:
         "Completed high school with distinction. Actively participated in student leadership roles and STEM-related initiatives, demonstrating a strong foundation for higher education.",
-      gpa: " - GPA: 4.0/4.0",
+      gpa: " - GPA: 3.99/4.0",
       SAT: " - SAT: 1480/1600",
       Achievements: "Achievements: ",
       EmSAT: " - EmSAT English & 1925/2000 & EmSAT Math : 1700/2000",

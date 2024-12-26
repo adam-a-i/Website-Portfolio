b676514a36a12869066afb04419e03d09c5e09a0
Adam Ibrahim <adamyehialy@gmail.com>
2024-12-26 19:12:42 +0400

    styling changes

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,20 +1,36 @@
 import React from 'react';
 import '../styles/projects.css';
 
-const Projects = ({title, }) => {
+const Projects = () => {
   return (
     <div className="projects">
-      <div className="cards">
-        <div className="card">
-          <div className="img"></div>
-          <span>AIExamify</span>
-          <p className="info">
-            AI-powered quiz generator that creates personalized quizzes and provides targeted learning recommendations like topics to review and youtube videos to watch!
-          </p>
-          <div className="share">
-          <div className="githubProjects">
-            <a href="#" aria-label="GitHub">
-              <svg
+  <div className="cards">
+    <div className="card">
+      <div className="img"></div>
+      <span>AI Chatbot using Reformers</span>
+      <p className="info">
+        Implemented Reformers with Trax in Python to develop an AI Chatbot.
+      </p>
+      <div className="share">
+        <div className="githubProjects">
+          <a href="#" aria-label="GitHub">
+            <svg /* SVG code here */></svg>
+          </a>
+        </div>
+        <span className="tools">Python</span>
+        <span className="tools">Machine Learning</span>
+      </div>
+    </div>
+  </div>
+</div>
+
+  );
+};
+
+export default Projects;
+
+
+<svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="16"
                 height="16"
@@ -24,15 +40,3 @@ const Projects = ({title, }) => {
               >
                 <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
               </svg>
-            </a>
-            </div>
-            <span className='tools'>React</span>
-            <span className='tools'>CSS</span>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-};
-
-export default Projects;
